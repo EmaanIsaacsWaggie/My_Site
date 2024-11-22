6dbbd879643ad35c0a10fec11f25e9ee6b4940f5
emaan <emaanwaggie@gmail.com>
2024-11-22 11:44:39 -1100

    Update conf.py with necessary Sphinx extensions for Django and Docker

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,13 @@ release = '01'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',      # Generates documentation from Python docstrings
+    'sphinx.ext.napoleon',     # Supports Google-style and NumPy-style docstrings
+    'sphinx.ext.viewcode',     # Adds links to source code
+    'sphinxcontrib_django',    # Django-specific tools for documenting models, views, etc. (install separately)
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
